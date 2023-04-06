dd2cc93e6b16ddd28ab0b7399b9261a1a79f67d0
evoxeed <nikita.gomozov@gmail.com>
2023-04-06 21:13:41 +0400

    init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import {createPinia} from "pinia";
+import './style.css'
+const pinia = createPinia()
+import App from './App.vue'
+
+createApp(App).use(pinia).mount('#app')
